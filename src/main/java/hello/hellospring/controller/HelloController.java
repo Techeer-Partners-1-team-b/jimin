103d7ca7ecd95913799bf45d9623c59929280f24
jiminsong490 <misosjm@naver.com>
2022-11-20 19:38:07 +0900

    섹션 5 회원 웹 기능 1

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,5 +1,7 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +11,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-
     @GetMapping("hello")
     public String hello(Model model){
         model.addAttribute("data","hello!!");
--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,5 +1,6 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,8 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MemberController {
     private final MemberService memberService;
 
+
     @Autowired
-    public MemberController(MemberService memberService) {
+    public MemberController(MemberService memberService)
+    {
         this.memberService = memberService;
     }
+
+
 }
